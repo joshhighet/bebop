f3e5f0792fee3a1377347ac71ce99e3897f80c78
josh! <github@joshhighet.com>
2023-01-31 19:44:47 +1300

    Create cryptocurrency.py

---
+++ app/cryptocurrency.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# https://raw.githubusercontent.com/joshhighet/bebop/main/assets/pagespider-test.html
+import re
+import logging
+import requests
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
+
+rex = {
+    'btc': re.compile(r'(bc1q[A-Za-z0-9]{39})|(([13]|bc1)[A-HJ-NP-Za-km-z1-9]{27,34}(?![A-Za-z0-9]))'),
+    'xmr': re.compile(r'([0-9AB]{1})([0-9a-zA-Z]{93})'),
+    'eth': re.compile(r'0x[a-fA-F0-9]{40}')
+}
+
+def getwallet_data(wallet, chain='btc'):
+    url = 'https://api.blockcypher.com/v1/%s/main/addrs/%s/balance' % (chain, wallet)
+    data = requests.get(url)
+    if data.status_code == 200:
+        logging.info('found %s wallet with balance %s (%s)', chain, data.json()['final_balance'], wallet)
+        return data.json()
+    logging.error('%s: failed to retrieve data on wallet - possibly a false match', wallet)
+    return None
+
+def main(sitesource):
+    findings = {key: [] for key in rex.keys()}
+    for currency, pattern in rex.items():
+        for match in re.finditer(pattern, sitesource):
+            findings[currency].append(match.group())
+    for currency, values in findings.items():
+        findings[currency] = list(set(values))
+        for value in findings[currency]:
+            logging.debug('found a potential %s wallet: %s', currency, value)
+            if currency == 'btc':
+                getwallet_data(value)
+            elif currency == 'eth':
+                getwallet_data(value, 'eth')
+            else:
+                logging.info('found suspected %s wallet: %s', currency, value)
+    return findings
