e48d43a87ed93f0dcb26365e5ed9b1e5aba20ac8
josh! <github@joshhighet.com>
2023-01-31 19:48:58 +1300

    add getcert

--- app/main.py
+++ app/main.py
@@ -14,7 +14,8 @@ import torscan
 import configcheck
 import opendir
 import shodansearch
-from utilities import preflight, getfqdn, getbaseurl, gen_chainconfig
+import getcert
+from utilities import preflight, getfqdn, getbaseurl, gen_chainconfig, validurl
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
 
@@ -22,26 +23,40 @@ parser = argparse.ArgumentParser()
 parser.add_argument('target', help='target address')
 args = parser.parse_args()
 
-preflight()
-url = args.target
-fqdn = getfqdn(url)
-url_base = getbaseurl(url)
+print(
+'''
+              __                     
+      _(\    |@@|                        __         __              
+     (__/\__ \--/ __                    / /_  ___  / /_  ____  ____ 
+        \___|----|  |   __             / __ \/ _ \/ __ \/ __ \/ __ \\
+            \ }{ /\ )_ / _\           / /_/ /  __/ /_/ / /_/ / /_/ /
+            /\__/\ \__O (__          /_.___/\___/_.___/\____/ .___/ 
+           (--/\--)    \__/                                /_/      
+           _)(  )(_                  
+          `---''---`                 hidden service safari 👀 🧅 💻 
+''')
 
+preflight()
+if not validurl(args.target):
+    logging.critical('failed to parse url - ensure a protocol is specified')
+    sys.exit(1)
+fqdn = getfqdn(args.target)
+url_base = getbaseurl(args.target)
 requestobject = getpage.main(args.target)
 if requestobject is None:
     logging.error('failed to retrieve page')
     sys.exit(1)
-if requestobject.status_code >= 400:
+if requestobject.status_code != 200:
     logging.warning('unexpected response code: %s', requestobject.status_code)
 title.main(requestobject)
 header_data = headers.main(requestobject)
-if header_data['etag'] is not None:
-    etag_correlations = shodansearch.query(header_data['etag'])
-favicon_data = favicon.main(url_base, requestobject)
-configcheck.main(args.target)
-torscan.main(fqdn)
+if args.target.startswith('https'):
+    getcert_data = getcert.main(args.target)
 pagespider_data = pagespider.main(args.target, requestobject)
+configcheck.main(args.target)
+favicon_data = favicon.main(url_base, requestobject)
 for item in pagespider_data['internal']:
     itemsource = getpage.main(item)
     if itemsource is not None:
         opendir.main(itemsource)
+torscan.main(fqdn)
