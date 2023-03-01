ce681eb4aa4c2b8c3906a848ba5c424a020a8a12
josh! <github@joshhighet.com>
2023-03-01 20:40:32 +1300

    define static uagt

--- app/utilities.py
+++ app/utilities.py
@@ -11,6 +11,8 @@ from urllib.parse import urlparse
 
 log = logging.getLogger(__name__)
 
+useragentstr = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.3 Safari/605.1.15'
+
 def nsresolve(fqdn):
     try:
         return socket.gethostbyname(fqdn)
