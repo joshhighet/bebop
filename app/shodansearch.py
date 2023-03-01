effb0fc160f40a801e1583c57a0533e63b8f683d
josh! <github@joshhighet.com>
2023-03-01 19:24:14 +1300

    Update shodansearch.py

--- app/shodansearch.py
+++ app/shodansearch.py
@@ -11,15 +11,18 @@ SHODAN_API_KEY = os.getenv('SHODAN_API_KEY', None)
 if SHODAN_API_KEY:
     api = shodan.Shodan(SHODAN_API_KEY)
 
-def query(query):
+def query(squery):
     findings = []
     if not SHODAN_API_KEY:
         log.error("shodan: without an api key queries are skipped")
         return findings
+    if not squery:
+        log.error("shodan: no query provided")
+        return findings
     try:
-        log.debug('shodan: querying %s', query)
-        results = api.search(query)
-        log.info('shodan: found %s results for %s', results['total'], query)
+        log.debug('shodan: querying %s', squery)
+        results = api.search(squery)
+        log.info('shodan: found %s results for %s', results['total'], squery)
         if results['total'] > 20:
             log.warning('shodan: a large number of findings here is abnormal. review results carefully!')
         for result in results['matches']:
