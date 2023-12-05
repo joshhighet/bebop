668f3b5f0b8c1275fe27f0a7b863d505de65737f
josh! <github@joshhighet.com>
2023-12-05 21:19:40 +1300

    fix tests for crypto, re-add into spidering workflow

--- app/cryptocurrency.py
+++ app/cryptocurrency.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python3
-# https://raw.githubusercontent.com/joshhighet/bebop/main/assets/pagespider-test.html
 import re
 import logging
-from bs4 import BeautifulSoup
-import getpage
 import requests
-
-from utilities import useragentstr
+from bs4 import BeautifulSoup
 
 log = logging.getLogger(__name__)
 
@@ -18,17 +14,20 @@ rex = {
 
 def getwallet_data(wallet, chain='btc'):
     url = 'https://api.blockcypher.com/v1/%s/main/addrs/%s/balance' % (chain, wallet)
-    data = getpage.main(url)
+    data = requests.get(url, timeout=10)
     if data.status_code == 200:
-        log.info('found %s wallet with balance %s (%s)', chain, data.json()['final_balance'], wallet)
+        bal = data.json()['final_balance']
+        log.info('found %s wallet with balance %s (%s)', chain, bal, wallet)
         return data.json()
     log.error('%s: failed to retrieve data about %s - likely a false match', chain, wallet)
     return None
 
 def main(sitesource):
+    log.debug('searching for cryptocurrency wallets')
     findings = {key: [] for key in rex.keys()}
     for currency, pattern in rex.items():
         for match in re.finditer(pattern, sitesource):
+            log.debug('found a potential %s wallet: %s', currency, match.group())
             findings[currency].append(match.group())
     for currency, values in findings.items():
         findings[currency] = list(set(values))
@@ -40,31 +39,39 @@ def main(sitesource):
                 getwallet_data(value, 'eth')
             else:
                 log.info('found suspected %s wallet: %s', currency, value)
+    if len(findings['btc']) == 0:
+        log.debug('no btc wallets found')
+    if len(findings['eth']) == 0:
+        log.debug('no eth wallets found')
+    if len(findings['xmr']) == 0:
+        log.debug('no xmr wallets found')
     return findings
 
 def walletexplorer_inspect_and_pivot(address):
     walletid = ""
     addresses = []
-    try:
-        ##walletnote
-        addresswe = f"https://www.walletexplorer.com/address/{address}"
-        pageaddr = requests.get(addresswe, headers={'User-Agent': useragentstr})
-        soup = BeautifulSoup(pageaddr.text, 'html.parser')
-        linkwallet = soup.find("div", {"class": "walletnote"}).find('a').get('href')
-        walletid =  linkwallet.split("/")
-        if(len(walletid)>0):
-            urlwalletaddress = f"https://www.walletexplorer.com/wallet/{walletid[2]}/addresses"
-            walletaddr = requests.get(urlwalletaddress, headers={'User-Agent': useragentstr})
-            soup = BeautifulSoup(walletaddr.text, 'html.parser')
-            addresstable = soup.find("table")
-            trs = addresstable.find_all('tr')
-            first = True
-            for tr in trs:
-                if not first:
-                    pivotaddress = tr.find("a").get("href").replace("/address/","")
-                    if pivotaddress != address:
-                        addresses.append(pivotaddress)
-                first = False
-    except Exception as exx:
-        log.error('%s: failed to retrieve data about %s - likely a false match', exx, address)
+    pageaddr = requests.get(f"https://www.walletexplorer.com/address/{address}", timeout=10)
+    if pageaddr.text.find(f"Address {address} not found") > -1:
+        log.warning('address %s not found, likely a false match', address)
+        return walletid, addresses
+    soup = BeautifulSoup(pageaddr.text, 'html.parser')
+    linkwallet = soup.find("div", {"class": "walletnote"}).find('a').get('href')
+    walletid =  linkwallet.split("/")
+    if(len(walletid)>0):
+        urlwalletaddress = f"https://www.walletexplorer.com/wallet/{walletid[2]}/addresses"
+        walletaddr = requests.get(urlwalletaddress, timeout=10)
+        soup = BeautifulSoup(walletaddr.text, 'html.parser')
+        addresstable = soup.find("table")
+        trs = addresstable.find_all('tr')
+        first = True
+        for tr in trs:
+            if not first:
+                pivotaddress = tr.find("a").get("href").replace("/address/","")
+                if pivotaddress != address:
+                    addresses.append(pivotaddress)
+            first = False
     return walletid, addresses
+
+#getwallet_data('1F1tAaz5x1HUXrCNLbtMDqcw6o5GNn4xqX', 'btc')
+#getwallet_data('0x00000000219ab540356cbb839cbe05303d7705fa', 'eth')
+#walletexplorer_inspect_and_pivot('bc1qa5wkgaew2dkv56kfvj49j0av5nml45x9ek9hz6')
--- tests/test_cryptocurrency.py
+++ tests/test_cryptocurrency.py
@@ -1,13 +1,61 @@
-import pytest
+import sys
+import os
+import unittest
+from unittest.mock import patch, MagicMock
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from app import cryptocurrency
-from app import getpage
+class TestYourScript(unittest.TestCase):
 
-@pytest.fixture
-def detail_to_group_basic():
-    return {
-        "address": "bc1qa5wkgaew2dkv56kfvj49j0av5nml45x9ek9hz6",
-    }
+    def setUp(self):
+        self.dummy_html = """<!DOCTYPE html>
+        <html>
+            <body>
+                <h1>cryptocurrency testextracts</h1>
+                    <p>litecoin</p>
+                        <address>LP98Q2gPZ9gUhoL5fDji357HPRHxVqWh6j</address>
+                        <address>ltc1qzvcgmntglcuv4smv3lzj6k8szcvsrmvk0phrr9wfq8w493r096ssm2fgsw</address>
+                    <p>monero</p>
+                        <address>888tNkZrPN6JsEgekjMnABU4TBzc2Dt29EPAvkRxbANsAnjyPbb3iQ1YBRk1UXcdRsiKc9dhwMVgN5S9cQUiyoogDavup3H</address>
+                    <p>btc</p>
+                        <address>1F1tAaz5x1HUXrCNLbtMDqcw6o5GNn4xqX</address>
+                        <address>34xp4vRoCGJym3xR7yCVPFHoCNxv4Twseo</address>
+                        <address>bc1qgdjqv0av3q56jvd82tkdjpy7gdp9ut8tlqmgrpmv24sq90ecnvqqjwvw97</address>
+                        <address>bc1qa5wkgaew2dkv56kfvj49j0av5nml45x9ek9hz6</address>
+                    <p>eth</p>
+                        <address>0x00000000219ab540356cbb839cbe05303d7705fa</address>
+            </body>
+        </html>
+        """
 
-def test_walletexplorer(detail_to_group_basic):
-    walletid, pivoted_address = cryptocurrency.walletexplorer_inspect_and_pivot(detail_to_group_basic["address"])
-    assert len(pivoted_address) == 0 and walletid
+def test_extract_wallet_addresses(self):
+    expected_btc_addresses = ['1F1tAaz5x1HUXrCNLbtMDqcw6o5GNn4xqX', '34xp4vRoCGJym3xR7yCVPFHoCNxv4Twseo', 
+                              'bc1qgdjqv0av3q56jvd82tkdjpy7gdp9ut8tlqmgrpmv24sq90ecnvqqjwvw97', 
+                              'bc1qa5wkgaew2dkv56kfvj49j0av5nml45x9ek9hz6']
+    expected_eth_addresses = ['0x00000000219ab540356cbb839cbe05303d7705fa']
+    result = your_script.main(self.dummy_html)
+    self.assertListEqual(result['btc'], expected_btc_addresses)
+    self.assertListEqual(result['eth'], expected_eth_addresses)
+
+@patch('your_script.getpage.main')
+def test_getwallet_data(self, mock_getpage):
+    mock_getpage.return_value = MagicMock(status_code=200, json=lambda: {'final_balance': 1000})
+    result = your_script.getwallet_data('1F1tAaz5x1HUXrCNLbtMDqcw6o5GNn4xqX')
+    self.assertIsNotNone(result)
+    self.assertEqual(result['final_balance'], 1000)
+    mock_getpage.return_value = MagicMock(status_code=404)
+    result = your_script.getwallet_data('non_existent_wallet')
+    self.assertIsNone(result)
+
+@patch('your_script.requests.get')
+def test_walletexplorer_inspect_and_pivot(self, mock_get):
+    mock_response = MagicMock()
+    mock_response.status_code = 200
+    mock_response.text = "Mocked HTML response with wallet info"
+    mock_get.return_value = mock_response
+    wallet_id, addresses = your_script.walletexplorer_inspect_and_pivot('1F1tAaz5x1HUXrCNLbtMDqcw6o5GNn4xqX')
+    self.assertNotEqual(wallet_id, "")
+    self.assertIsInstance(addresses, list)
+
+if __name__ == '__main__':
+    unittest.main()
