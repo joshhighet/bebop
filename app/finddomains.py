cf2ccf96056f5e491933861dcc5f521c0db92928
josh! <github@joshhighet.com>
2025-07-13 09:05:16 +1200

    Update finddomains.py

--- app/finddomains.py
+++ app/finddomains.py
@@ -2,7 +2,9 @@ import socket
 import socks
 import logging
 import subprocessors
-
+import dns.resolver
+import dns.name
+import dns.rdatatype
 from utilities import getproxyvalue
 
 log = logging.getLogger(__name__)
@@ -10,34 +12,41 @@ log = logging.getLogger(__name__)
 def get_proxy_socket():
     proxyaddr, proxyport = getproxyvalue()
     socks.set_default_proxy(socks.SOCKS5, proxyaddr, proxyport)
+    socket.socket = socks.socksocket
     return socks.socksocket()
 
 def reverse_dns_lookup(ip_address):
     try:
-        # Use a local proxy-enabled socket
-        with get_proxy_socket() as s:
-            hostname, _, _ = s.gethostbyaddr(ip_address)
-        return hostname
+        reverse_name = dns.reversename.from_address(ip_address)
+        resolver = dns.resolver.Resolver()
+        answers = resolver.resolve(reverse_name, 'PTR')
+        return str(answers[0])[:-1]
+    except dns.resolver.NoAnswer:
+        logging.debug(f"no rDNS record for {ip_address}")
+        return None
     except Exception as e:
         logging.error(f"error in rDNS lookup for {ip_address}: {e}")
         return None
 
 def check_hostname_resolvematch(hostnames, target_ip):
     resolved_hostnames = []
+    resolver = dns.resolver.Resolver()
     for hostname in hostnames:
         try:
-            # Use a local proxy-enabled socket
-            with get_proxy_socket() as s:
-                ip = s.gethostbyname(hostname)
-            if ip == target_ip:
-                resolved_hostnames.append(hostname)
+            answers = resolver.resolve(hostname, 'A')
+            for rdata in answers:
+                if str(rdata) == target_ip:
+                    resolved_hostnames.append(hostname)
+                    break
+        except dns.resolver.NXDOMAIN:
+            logging.debug(f"no resolution for {hostname}")
         except Exception as e:
             logging.debug(f"failed to resolve {hostname}: {e}")
     return resolved_hostnames
 
 def main(ip):
     log.info('starting finddomains')
-    reverse_dns_hostname = reverse_dns_lookup(target_ip)
+    reverse_dns_hostname = reverse_dns_lookup(ip)
     log.info('querying virustotal for resolutions')
     vtires = subprocessors.query_resolutions_virustotal(ip)
     log.info('querying urlscan for resolutions')
